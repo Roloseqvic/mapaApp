8cadcf8becb266d53433cea12bf1e35c1ef8df86
Roloseqvic <89818164+Roloseqvic@users.noreply.github.com>
2022-05-27 19:44:30 -0600

    Add files via upload

---
+++ js/mapascript.js
@@ -0,0 +1,38 @@
+//definir la variable global
+var map;
+
+//mostrar el mapa
+cargarMapa([9.880387,-83.923898]);
+
+
+//funcion cargarmapa
+function cargarMapa(coord) {
+    map = L.map('mapa').setView(coord, 18);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+
+    //asignar el evento manejador de locationfound al mapa
+    map.on('locationfound', onLocationFound);
+
+    //iniciar recorrido
+    recorrido();
+}
+
+
+//funcion manejadora del evento locationfound
+function onLocationFound(e) {
+    console.log(e);
+    var radius = e.accuracy;
+    L.marker(e.latlng).addTo(map);
+}
+
+
+function recorrido() {
+    //invocamos el metodo locate para definir el view en la 
+    //ubicación del gps
+    map.locate({setView: true, maxZoom: 16});
+
+    setTimeout(recorrido,3000);
+}
